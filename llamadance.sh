bb569eadd6d3810991dfb9527816a78e75eb056d
Ben <bjb@deus.net>
2019-03-12 09:15:28 +0000

    add a dancing llama

---
+++ llamadance.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+
+
+while true ; do
+
+clear ; cat << EOT
+
+  \/
+ <'l
+  ll
+  llama~
+  || ||
+  '' ''
+EOT
+
+sleep 0.1
+
+clear ; cat << EOT
+  //
+ <-l
+  ll
+  llama~
+  zz zz
+
+EOT
+
+sleep 0.2
+
+done
